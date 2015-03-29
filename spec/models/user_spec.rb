55b7c5636560981628f0e92ac6222e759ac1494e
GOGEN <gogen.marat@gmail.com>
2015-03-29 13:04:08 +0300

    finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -51,7 +51,7 @@ describe User do
 	  specify { expect(user_for_invalid_password).to be_false }
 	end
   end
-  
+
   describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
@@ -99,5 +99,15 @@ describe User do
     end
 
     it { should_not be_valid }
+  end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
   end
 end
